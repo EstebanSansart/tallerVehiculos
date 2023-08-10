175fe111b141cb3a429901dacf47929f7d64972f
EstebanSansart <julianestebansanabria@gmail.com>
2023-08-09 19:02:26 -0500

    feat: :white_check_mark: Test Successful
    WeatherForecastController modificado para probar el proyecto

--- TallerApi/Controllers/WeatherForecastController.cs
+++ TallerApi/Controllers/WeatherForecastController.cs
@@ -6,27 +6,9 @@ namespace TallerApi.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+    [HttpGet]
+    public IEnumerable<string> Get(){
+        string[] nombres = new[]{"Fabian", "Rolando", "Tatiana", "Luis", "Andres", "Karen", "Javier", "Kevin", "Esteban"};
+        return nombres;
     }
 }
